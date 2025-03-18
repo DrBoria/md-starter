0777b30cb03f4f3580d1b29170727f75f5c2bd09
Mikita Dusmikeev <dr.boria@gmail.com>
2025-03-18 12:24:41 +0100

    fixed landing page

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,35 +1,64 @@
+const { withPlugins } = require('next-compose-plugins');
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   compiler: {
-    styledComponents: true,
+    styledComponents: true
   },
   experimental: {
-    esmExternals: 'loose',
+    esmExternals: 'loose'
   },
   images: {
-    loader: "custom",
+    loader: 'custom',
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  transpilePackages: ["next-image-export-optimizer", "react-daypicker", "date-fns"],
-  env: {
-    nextImageExportOptimizer_imageFolderPath: "public/images",
-    nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: "75",
-    nextImageExportOptimizer_storepicturesinWEBP: "true",
-    nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
-    nextImageExportOptimizer_generateAndUseBlurImages: "true",
-    nextImageExportOptimizer_RemoteImageCacheTTL: "0",
-  },
-  webpack(config) {
+  transpilePackages: [
+    '@md/styles',
+    '@md/components',
+    '@md/sections',
+    '@md/api',
+    'next-image-export-optimizer'
+  ],
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@md/styles': path.resolve(__dirname, '../../packages/styles'),
+        'styled-components': path.resolve(__dirname, 'node_modules/styled-components')
+      },
+      modules: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, '../../packages/styles'),
+        'node_modules'
+      ]
+    };
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts', '.tsx', '.mjs'],
+      '.mjs': ['.mts', '.mjs', '.js']
+    };
+
+    // Специальное правило для next-image-export-optimizer
+    config.module.rules.push({
+      test: /next-image-export-optimizer/,
+      type: "javascript/auto",
+      resolve: {
+        fullySpecified: false
+      }
+    });
+
     config.module.rules.push({
       test: /\.svg$/,
       issuer: { and: [/\.(js|ts)x?$/] },
       use: [{ loader: 'raw-loader' }],
     });
+
     return config;
-  },
+  }
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([], nextConfig);
